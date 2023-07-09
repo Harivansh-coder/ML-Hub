801c9462024d45420c2e4cd8bd86c0078bfabbf2
Harivansh-coder <harivanshthakur8@gmail.com>
2023-07-10 00:24:36 +0530

    implmented completely the login route :)

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -1,33 +1,45 @@
 # this file contains the script for the authentication of the user
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from app.models.user import UserLogin
+from app.models.token import Token
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.schemas.user import User
+from app.models.auth import EmailPasswordOAuth2PasswordRequestForm
+from app.utils.verify_hash import verify_password
+from app.utils.oauth2 import create_access_token
 
 # router for the authentication of the user
 router = APIRouter(
-    prefix="/auth/login",
-    tags=["auth"],
+    prefix="/auth",
+    tags=["authencation"],
     responses={404: {"description": "Not found"}},
 )
 
 
 # login route for the user
-@router.post("/", status_code=status.HTTP_200_OK)
-async def login_user(user: UserLogin, db: Session = Depends(get_db)):
+@router.post("/login", status_code=status.HTTP_200_OK, response_model=Token)
+async def login_user(
+    user_credentials: EmailPasswordOAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
     # get user from database
-    user = (
-        db.query(User)
-        .filter(User.email == user.email, User.password == user.password)
-        .first()
-    )
+    user = db.query(User).filter(User.email == user_credentials.email).first()
 
-    # raise exception if user does not exist
-    if user is None:
+    # check if user exists
+    if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Email not registered"
         )
 
-    return {"data": user}
+    # check if password is correct
+    if not verify_password(user_credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password"
+        )
+
+    # create access token
+    access_token = create_access_token(data={"user_id": user.id})
+
+    # return access token
+    return {"access_token": access_token, "token_type": "bearer"}
